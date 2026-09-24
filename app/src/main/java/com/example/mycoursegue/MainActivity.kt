7bd270f647eb7f45041a44b1ce039b1b32e63fd3
hafidhdzaki <24hafidhdzaki@gmail.com>
2026-09-24 09:23:23 +0700

    feat: option menu

--- app/src/main/java/com/example/mycoursegue/MainActivity.kt
+++ app/src/main/java/com/example/mycoursegue/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mycoursegue
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,29 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_home -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_materi -> {
+                // Logika untuk melihat skor
+                Toast.makeText(this, "Materi", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_quiz -> {
+                Toast.makeText(this, "Quiz", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
